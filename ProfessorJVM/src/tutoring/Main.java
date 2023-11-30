10b88750b3e3bc037a42732c0d5f63a38934e497
Alaeddine-Nasri <ia_nasri@esi.dz>
2023-11-30 19:44:52 +0100

    Sessions Page ProfessorJVM

--- ProfessorJVM/src/tutoring/Main.java
+++ ProfessorJVM/src/tutoring/Main.java
@@ -28,11 +28,18 @@ import javafx.stage.Stage;
 public class Main  extends Application{
 	
 	 private IProfessorList professorList; 
+	 IProfessor prof;
 	 @Override
 	    public void start(Stage primaryStage) {
 	        primaryStage.setTitle("Simple JavaFX App");
 
 	        // Create labels and text fields for tutor name and family name
+	        Label title = new Label("GustaveTutoring");
+	        title.setStyle("-fx-font-size: 22px; -fx-font-weight: bold;-fx-font-family: 'Sans-serif'; -fx-text-fill: #041444;");
+	        Label description = new Label("GustaveTutoring is an innovative tutoring application designed to provide a seamless and personalized learning experience for students seeking academic assistance. ");
+	        description.getStyleClass().add("desc");
+	        description.setWrapText(true); 
+	        description.setStyle("-fx-text-alignment: justify;-fx-line-spacing: 1.2;");
 	        Label tutorLabel = new Label("Tutor name");
 	        tutorLabel.getStyleClass().add("label");
 	        Label familyLabel = new Label("Family name");
@@ -53,36 +60,29 @@ public class Main  extends Application{
 	        moduleComboBox.getStyleClass().add("combo-box");
 	        
 	        
-
-	        Button submitButton = new Button("Submit");
+	        
+	        Button submitButton = new Button("Register");
 	        submitButton.setOnAction(e -> {
 	            String tutorName = tutorTextField.getText();
 	            String familyName = familyTextField.getText();
-	            Modules selectedModule = moduleComboBox.getSelectionModel().getSelectedItem(); 
+	            Modules selectedModule = moduleComboBox.getSelectionModel().getSelectedItem();
 
 	            if (selectedModule != null) {
-	                    
-	                    try {
-	                    	professorList = (IProfessorList) Naming.lookup("rmi://127.0.0.1:1101/professor_list");
-	            			System.out.println("Enroll prof");
-	            			EnumSet<Modules> modules = EnumSet.of(selectedModule);
-	            			 IProfessor prof = professorList.addProf(tutorName + " " + familyName, modules);
-		                        System.out.println("Prof " + prof.getName() + " with id  " + prof.getUUID() + " is registered.");
-	            			
-	            			// TODO Auto-generated catch block
-	            		}  catch (MalformedURLException ee) {
-	            			// TODO Auto-generated catch block
-	            			ee.printStackTrace();
-	            		} catch (Exception ee) {
-	            			// TODO Auto-generated catch block
-	            			ee.printStackTrace();
-	            		} 
-	                
+	                try {
+	                    professorList = (IProfessorList) Naming.lookup("rmi://127.0.0.1:1101/professor_list");
+	                    System.out.println("Enroll prof");
+	                    EnumSet<Modules> modules = EnumSet.of(selectedModule);
+	                    prof = professorList.addProf(tutorName + " " + familyName, modules);
+	                    System.out.println("Prof " + prof.getName() + " with id  " + prof.getUUID() + " is registered." + "sessions" + prof.getSessions());
+	                } catch (MalformedURLException ee) {
+	                    ee.printStackTrace();
+	                } catch (Exception ee) {
+	                    ee.printStackTrace();
+	                }
 
-	                String greeting = "Hello, " + tutorName + " " + familyName + "!\nSelected Module: " + selectedModule;
-	                label.setText(greeting);
-	                WelcomePage welcomePage = new WelcomePage(tutorName);
+	                WelcomePage welcomePage = new WelcomePage(prof);
 	                Scene welcomeScene = new Scene(welcomePage, 1000, 600);
+	                welcomeScene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
 
 	                // Set the new scene for the primary stage
 	                primaryStage.setScene(welcomeScene);
@@ -94,6 +94,8 @@ public class Main  extends Application{
 	        submitButton.getStyleClass().add("button");
 
 	        // Create an HBox layout and add the labels, text fields, and button to it
+	        VBox titlebox = new VBox(5);
+	        titlebox.getChildren().addAll(title,description);
 	        VBox Tutorbox = new VBox(5);
 	        Tutorbox.getChildren().addAll(tutorLabel, tutorTextField);
 	        VBox Familybox = new VBox(5);
@@ -119,15 +121,16 @@ public class Main  extends Application{
 	        imageVBox.setEffect(blur);
 	        // Create a VBox layout for the form
 	        VBox formVBox = new VBox(20);
-	        formVBox.setAlignment(Pos.CENTER);
-	        formVBox.getChildren().addAll(Tutorbox, Familybox, Modulebox, Buttonbox, label);
+	        formVBox.setAlignment(Pos.CENTER_LEFT);
+	        formVBox.getChildren().addAll(titlebox,Tutorbox, Familybox, Modulebox, Buttonbox, label);
 	        formVBox.setPadding(new Insets(10, 20, 10, 20));
 	        
 
 	        // Create a SplitPane to horizontally split the image and form
 	        SplitPane splitPane = new SplitPane(imageVBox, formVBox);
 	        splitPane.setDividerPositions(0.55); // Set the divider position (40% split)
-
+	        splitPane.setStyle("-fx-border-color: null;");
+	        
 	        // Create a scene with the SplitPane
 	        Scene scene = new Scene(splitPane, 1000, 600);
 	        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
--- ProfessorJVM/src/tutoring/WelcomePage.java
+++ ProfessorJVM/src/tutoring/WelcomePage.java
@@ -1,25 +1,161 @@
-// WelcomePage.java
 package tutoring;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
 public class WelcomePage extends VBox {
 
     private Label welcomeLabel;
 
-    public WelcomePage(String tutorName) {
-        initializeUI(tutorName);
+    public WelcomePage(IProfessor professor) {
+        initializeUI(professor);
     }
 
-    private void initializeUI(String tutorName) {
-        welcomeLabel = new Label("Welcome, " + tutorName + "!");
-        welcomeLabel.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;");
+    private void initializeUI(IProfessor professor) {
 
-        setAlignment(Pos.CENTER);
-        setPadding(new Insets(20));
-        getChildren().add(welcomeLabel);
+        HBox topBar = new HBox();
+        topBar.setAlignment(Pos.CENTER_LEFT);
+        topBar.setPadding(new Insets(10, 20, 10, 20));
+
+        Label projectNameLabel = new Label("Tutoring");
+        projectNameLabel.getStyleClass().add("title");
+        topBar.getChildren().add(projectNameLabel);
+
+        HBox professorInfo = new HBox(10);
+        professorInfo.setAlignment(Pos.CENTER_RIGHT);
+
+        VBox imageVBox = new VBox(20);
+        imageVBox.setAlignment(Pos.CENTER_RIGHT);
+        imageVBox.getStyleClass().add("avatar");
+
+        VBox sessions = new VBox(10);
+        sessions.setAlignment(Pos.CENTER);
+        Label professorNameLabel = new Label("Professor: ");
+        try {
+            String welcomeMessage = "Welcome, " + professor.getName() + "!\n";
+            welcomeMessage += "ID: " + professor.getUUID() + "\n";
+
+            professorNameLabel = new Label(professor.getName());
+            professorNameLabel.setStyle("-fx-font-size: 14px;");
+
+            welcomeLabel = new Label(welcomeMessage);
+            welcomeLabel.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;");
+            
+
+            if (professor.getSessions() != null && !professor.getSessions().isEmpty()) {
+                // Display sessions
+                for (ISession session : professor.getSessions()) {
+                    Label sessionLabel = new Label("Session: " + session.toString());
+                    sessions.getChildren().add(sessionLabel);
+                }
+            } else {
+                // Show message and button to add sessions
+                Label noSessionLabel = new Label("No sessions available. Add sessions below:");
+                Button addSessionButton = new Button("Add Session");
+                addSessionButton.setOnAction(e -> {
+                    try {
+                    	 boolean sessionCreated = professor.createSession(Modules.Algorithmic,Level.BEGINNER,1,LocalTime.of(9, 0),LocalTime.of(9, 0),LocalDate.of(2023, 12, 1),20,Currency.EUR);
+                    	  if (sessionCreated) {
+                    		  System.out.println("Session Created");
+                              // or notify the user that the session was added successfully
+                          } else {
+                        	  System.out.println("Session Error");
+                          }
+                    
+                    } catch (Exception ex) {
+                        ex.printStackTrace(); // Handle RemoteException as needed
+                    }
+                });
+                sessions.getChildren().addAll(noSessionLabel, addSessionButton);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        professorInfo.getChildren().addAll(professorNameLabel, imageVBox);
+
+        HBox header = new HBox(10);
+        header.setAlignment(Pos.CENTER);
+        header.getChildren().addAll(topBar, professorInfo);
+        header.getStyleClass().add("header");
+
+        
+
+        SplitPane splitPane = new SplitPane(createMenu(), sessions);
+        splitPane.setDividerPositions(0.2);
+
+        HBox mainPage = new HBox(10);
+        mainPage.setAlignment(Pos.CENTER);
+        mainPage.getChildren().addAll(splitPane);
+        mainPage.getStyleClass().add("mainpage");
+        splitPane.setStyle("-fx-border-color: null;-fx-background-color: #F5F5F5;");
+
+        HBox.setHgrow(topBar, Priority.ALWAYS);
+        HBox.setHgrow(splitPane, Priority.ALWAYS);
+
+        VBox fullPage = new VBox(10);
+        fullPage.setAlignment(Pos.TOP_CENTER);
+        fullPage.getChildren().addAll(header, mainPage);
+
+        setAlignment(Pos.TOP_CENTER);
+        setPadding(new Insets(0));
+        getChildren().addAll(fullPage);
     }
+
+    private VBox createMenu() {
+        Label menuItem = createMenuItem("All Sessions");
+        Label menuItem2 = createMenuItem("Next Sessions");
+        Label menuItem3 = createMenuItem("Past Sessions");
+
+        VBox menu = new VBox(5);
+        menu.setAlignment(Pos.TOP_LEFT);
+        menu.getChildren().addAll(menuItem, menuItem2, menuItem3);
+        menu.getStyleClass().add("menu");
+
+        // Set the first menu item as selected by default
+        selectMenuItem(menuItem);
+
+        return menu;
+    }
+
+    private Label createMenuItem(String text) {
+        Label menuItem = new Label(text);
+        menuItem.getStyleClass().add("menuItem");
+
+        // Set the label to take full width
+        menuItem.setMaxWidth(Double.MAX_VALUE);
+        menuItem.setAlignment(Pos.CENTER_LEFT);
+        menuItem.setStyle("-fx-pref-height: 30;");
+
+        // Event handling for mouse click
+        menuItem.setOnMouseClicked(event -> {
+            // Reset styles for all menu items
+            for (javafx.scene.Node node : ((VBox) menuItem.getParent()).getChildren()) {
+                node.getStyleClass().remove("selected");
+            }
+
+            // Apply selected style to the clicked menu item
+            menuItem.getStyleClass().add("selected");
+        });
+
+        return menuItem;
+    }
+
+
+    private void selectMenuItem(Label menuItem) {
+        // Apply selected style to the specified menu item
+        menuItem.getStyleClass().add("selected");
+    }
+
+
 }
